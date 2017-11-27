440be0ab58d648ee8f14f201af504b238bef43c7
Amit <chhabraamit.nsit@gmail.com>
2017-11-27 19:22:09 +0530

    Add commandline file flag

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	"bufio"
 	"os"
 
+	"flag"
+
 	"github.com/quiz/solution"
 )
 
 func main() {
+	file := flag.String("file", "problems.csv", "file containing quiz questions")
+	if *file == "problems.csv" {
+		file = flag.String("f", "problems.csv", "file containing quiz questions")
+	}
+	flag.Parse()
+
 	quiz := solution.MyQuiz{
-		File:   "problems.csv",
+		File:   *file,
 		Reader: bufio.NewReader(os.Stdin),
 	}
 
