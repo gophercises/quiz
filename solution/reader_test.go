cd0917f6e97be88dedc43932d219f2d372184cf8
Amit <chhabraamit.nsit@gmail.com>
2017-11-29 00:42:36 +0530

    Add tests

---
+++ solution/reader_test.go
@@ -0,0 +1,36 @@
+package solution
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+func TestReadCsv(t *testing.T) {
+	name := "test.csv"
+	f := createFile(name)
+
+	dat := ReadCsv(name)
+	if len(dat) != 2 {
+		log.Println("Map size differs than expected in readfile")
+		t.Fail()
+	}
+
+	if dat["kOne"] != "valOne" && dat["kTwo"] != "valTwo" {
+		log.Println("Fail while reading file content")
+		t.Fail()
+	}
+	f.Close()
+}
+func createFile(name string) *os.File {
+	f, err := os.Create(name)
+	if err != nil {
+		panic(err)
+	}
+	f.WriteString("kOne,valOne,")
+	f.WriteString("\n")
+	f.WriteString("kTwo,valTwo,")
+	f.WriteString("\n")
+
+	return f
+}
