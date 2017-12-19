53560f10a0fac93de1d00e069b545e62dc110df1
John Kamenik <jkamenik@fourv.com>
2017-12-18 21:05:01 -0500

    Adding a timer.

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	problemsFilename := flag.String("c", "problems.csv", "The file containing the problems")
+	timeLimit := flag.Int("l", 30, "The time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*problemsFilename)
@@ -25,17 +27,31 @@ func main() {
 		os.Exit(1)
 	}
 
-	var text string
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	answers := make(chan string)
+
 	correct := 0
 
 	for _, line := range lines {
 		question := strings.TrimSpace(line[0])
 		answer := strings.TrimSpace(line[1])
 		fmt.Printf("%s? ", question)
-		fmt.Scanf("%s\n", &text)
 
-		if text == answer {
-			correct++
+		go func() {
+			var text string
+			fmt.Scanf("%s\n", &text)
+			answers <- text
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("Times up!\n")
+			fmt.Printf("You got %d correct output of %d\n", correct, len(lines))
+			return
+		case text := <-answers:
+			if text == answer {
+				correct++
+			}
 		}
 	}
 
