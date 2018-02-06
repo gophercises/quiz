487f0902b11e7747bd02ddba70c537840d3865a2
Anand <kannanenator@gmail.com>
2018-02-05 21:59:19 -0500

    actually EOF newline

--- students/kannanenator/main.go
+++ students/kannanenator/main.go
@@ -13,7 +13,6 @@ func main() {
 	
 	filenamePtr := flag.String("filename", "problems.csv", "file containing the set of problems")
 	limitPtr := flag.Int("limit", 30, "quiz time limit")
-	shufflePtr := flag.Bool("shuffle", false, "shuffle the quiz if true")
 
 	flag.Parse()
 
