4b472293abe5f7742f170a52f2dc1d2bf5dbe9e7
Brandon <brandoncpoole@gmail.com>
2018-04-15 00:56:21 -0700

    Added timelimit to quiz with a prompt before starting.

--- quiz.go
+++ quiz.go
@@ -7,28 +7,52 @@ import (
 	"io"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
 	file, _ := os.Open("./problems.csv")
 	r := csv.NewReader(io.Reader(file))
 
-	data, _ := r.ReadAll()
+	problems, _ := r.ReadAll()
 
+	fmt.Println("Press ENTER to start...")
+	startReader := bufio.NewReader(os.Stdin)
+	startReader.ReadString('\n')
+	runQuiz(problems)
+}
+
+func runQuiz(problems [][]string) {
 	correct := 0
-	total := len(data)
-	for _, problem := range data {
-		question := problem[0]
-		answer := problem[1]
-		fmt.Printf("%s: ", question)
+	total := len(problems)
 
-		reader := bufio.NewReader(os.Stdin)
-		guess, _ := reader.ReadString('\n')
-		guess = strings.TrimSpace(guess)
-		if strings.Compare(answer, guess) == 0 {
-			correct++
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		timer := time.NewTimer(30 * time.Second)
+		<-timer.C
+		fmt.Printf("\nTime's up!\n")
+		wg.Done()
+	}()
+
+	go func() {
+		for _, problem := range problems {
+			question := problem[0]
+			answer := problem[1]
+			fmt.Printf("%s: ", question)
+
+			r := bufio.NewReader(os.Stdin)
+			guess, _ := r.ReadString('\n')
+			guess = strings.TrimSpace(guess)
+			if strings.Compare(answer, guess) == 0 {
+				correct++
+			}
 		}
-	}
+		wg.Done()
+	}()
 
+	wg.Wait()
 	fmt.Printf("You answered %d of %d correct", correct, total)
 }
