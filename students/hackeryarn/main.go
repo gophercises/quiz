272e89060ffe0dabf09be686692908e0d634a38a
achernyak <artemchernyak@gmail.com>
2018-05-23 14:29:26 -0500

    supporting flags

--- students/hackeryarn/main.go
+++ students/hackeryarn/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"io"
 	"log"
 	"os"
@@ -9,6 +10,26 @@ import (
 	"github.com/gophercises/quiz/students/hackeryarn/problem"
 	"github.com/gophercises/quiz/students/hackeryarn/quiz"
 )
+
+const (
+	// FileFlag is the flat to be used to set a file
+	FileFlag = "file"
+	// FileFlagValue is the value that is used when no FileFlag is provided
+	FileFlagValue = "problems.csv"
+	// FileFlagUsage is the help string for the FileFlag
+	FileFlagUsage = "Questions file"
+)
+
+// Flagger configures the flags used
+type Flagger interface {
+	StringVar(p *string, name, value, usage string)
+}
+
+type quizFlagger struct{}
+
+func (q *quizFlagger) StringVar(p *string, name, value, usage string) {
+	flag.StringVar(p, name, value, usage)
+}
 
 // ReadCSV parses the CSV file into a Problem struct
 func ReadCSV(reader io.Reader) quiz.Quiz {
@@ -29,8 +50,22 @@ func ReadCSV(reader io.Reader) quiz.Quiz {
 	return quiz.New(problems)
 }
 
+var file string
+
+// ConfigFlags sets all the flags used by the application
+func ConfigFlags(f Flagger) {
+	f.StringVar(&file, FileFlag, FileFlagValue, FileFlagUsage)
+}
+
+func init() {
+	flagger := &quizFlagger{}
+	ConfigFlags(flagger)
+
+	flag.Parse()
+}
+
 func main() {
-	file, err := os.Open("problems.csv")
+	file, err := os.Open(file)
 	if err != nil {
 		log.Fatalln("Could not open file", err)
 	}
--- students/hackeryarn/quiz_test.go
+++ students/hackeryarn/quiz_test.go
@@ -9,6 +9,20 @@ import (
 	"github.com/gophercises/quiz/students/hackeryarn/quiz"
 )
 
+type flaggerMock struct {
+	stringVarCalls  int
+	stringVarNames  []string
+	stringVarValues []string
+	stringVarUsages []string
+}
+
+func (f *flaggerMock) StringVar(p *string, name, value, usage string) {
+	f.stringVarCalls++
+	f.stringVarNames = append(f.stringVarNames, name)
+	f.stringVarValues = append(f.stringVarValues, value)
+	f.stringVarUsages = append(f.stringVarUsages, usage)
+}
+
 func TestReadCSV(t *testing.T) {
 	input := "7+3,10\n1+1,2"
 	reader := bytes.NewBufferString(input)
@@ -24,6 +38,38 @@ func TestReadCSV(t *testing.T) {
 	got := ReadCSV(reader)
 
 	if !reflect.DeepEqual(want, got) {
-		t.Errorf("Expected to read in %v got %v", want, got)
+		t.Errorf("it should read in %v got %v", want, got)
+	}
+}
+
+func TestConfigFlags(t *testing.T) {
+	flagger := &flaggerMock{}
+
+	ConfigFlags(flagger)
+
+	assertStringVars(t, flagger)
+}
+
+func assertStringVars(t *testing.T, flagger *flaggerMock) {
+	t.Helper()
+
+	if flagger.stringVarCalls != 1 {
+		t.Errorf("it should call StringVars %d times, called %d", 1, flagger.stringVarCalls)
+	}
+
+	expectedNames := []string{FileFlag}
+	expectedValues := []string{FileFlagValue}
+	expectedUsages := []string{FileFlagUsage}
+
+	if !reflect.DeepEqual(expectedNames, flagger.stringVarNames) {
+		t.Errorf("it should setup StringVar names to be %v, got %v", expectedNames, flagger.stringVarNames)
+	}
+
+	if !reflect.DeepEqual(expectedValues, flagger.stringVarValues) {
+		t.Errorf("it should setup StringVar values to be %v, got %v", expectedValues, flagger.stringVarValues)
+	}
+
+	if !reflect.DeepEqual(expectedUsages, flagger.stringVarUsages) {
+		t.Errorf("it should setup StringVar usages to be %v, got %v", expectedUsages, flagger.stringVarUsages)
 	}
 }
