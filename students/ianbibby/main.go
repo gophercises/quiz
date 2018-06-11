e1ed7aeb784caf8fbcf83a5d5a6d78e1ef9c5c5f
Ian Bibby <ian.bibby@gmail.com>
2018-06-10 23:39:43 -0700

    Updated quiz to use an actor pattern

--- students/ianbibby/main.go
+++ students/ianbibby/main.go
@@ -1,6 +1,29 @@
+/*
+	The quiz accepts a CSV file and allows for a custom timeout and
+	randomized shuffling of questions.
+
+	The implementation is based on an actor model where the defined
+	actions are:
+
+		- AskQuestion
+			- Prints the question.
+			- Sends notification to listen for user input.
+		- QuestionAnswered
+			- Store the given answer.
+			- Sends notification that an answer has been handled.
+		- PrintSummary
+			- Compares answers and prints the results.
+
+	The timeout is achieved using a context, where a deadline is set
+	whenever the timeout is > 0.
+
+	The actor loop is terminated when either the last question has
+	been answered, or the timeout has occured.
+*/
 package main
 
 import (
+	"context"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -20,46 +43,23 @@ type qa struct {
 
 // Quiz stores the state of the quiz
 type Quiz struct {
-	qas          []*qa
-	timeout      int
-	done         chan bool
-	question     chan *qa
-	ask          chan *qa
-	answered     chan bool
-	correctCount int
-	askedCount   int
+	ctx      context.Context
+	ops      chan func() bool
+	qas      []*qa
+	answered map[*qa]string
 }
 
 // NewQuiz instatiates a new Quiz struct
-func NewQuiz(timeout int) *Quiz {
+func NewQuiz(ctx context.Context) *Quiz {
 	return &Quiz{
-		timeout:  timeout,
-		done:     make(chan bool),
-		question: make(chan *qa),
-		ask:      make(chan *qa),
-		answered: make(chan bool),
+		ctx:      ctx,
+		ops:      make(chan func() bool),
+		answered: map[*qa]string{},
 	}
 }
 
-// timer starts a timer loop, for timeout control
-func (q *Quiz) timer() {
-	defer close(q.done)
-
-	for {
-		select {
-		case <-time.After(time.Duration(q.timeout) * time.Second):
-			fmt.Print("\tTime's up!\n")
-			q.done <- true
-			return
-		case <-q.done:
-			fmt.Println("Done")
-			return
-		}
-	}
-}
-
-// readFile reads question data into an array
-func (q *Quiz) readFile(filename string) {
+// LoadFromFile reads question data into an array
+func (q *Quiz) LoadFromFile(filename string) {
 	csvData, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("failed to open file: %v", err)
@@ -80,50 +80,6 @@ func (q *Quiz) readFile(filename string) {
 	}
 }
 
-// processQuestions loops through the questions array,
-// sending each question to the ask channel, then
-// waiting to be notified of an answer
-func (q *Quiz) processQuestions() {
-	defer close(q.ask)
-
-	for _, question := range q.qas {
-		q.ask <- question
-		select {
-		case <-q.answered:
-			// Wait until the question has been answered before moving on
-		case <-q.done:
-			// Canceled, possibly due to a timeout
-			return
-		}
-	}
-
-	q.done <- true
-}
-
-// askQuestion ranges over the ask channel for questions to be asked,
-// then waits for an answer over stdin, before sending an answered
-// notification
-func (q *Quiz) askQuestion() {
-	defer close(q.answered)
-
-	for qa := range q.ask {
-		fmt.Printf("Question #%-3d %s= ", q.askedCount+1, qa.question)
-		q.askedCount++
-
-		var answer string
-		fmt.Fscanln(os.Stdin, &answer)
-		if strings.TrimSpace(answer) == qa.answer {
-			q.correctCount++
-		}
-		q.answered <- true
-	}
-}
-
-// printSummary prints a summary of the quiz play
-func (q *Quiz) printSummary() {
-	fmt.Printf("\nYou answered %d/%d questions correctly!\n", q.correctCount, q.askedCount)
-}
-
 // shuffle shuffles the question order
 func (q *Quiz) shuffle() {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -132,21 +88,112 @@ func (q *Quiz) shuffle() {
 	})
 }
 
+// ReadInput runs a loop that waits for input and blocks.
+// To avoid unnecessary blocking, there are 2 conditions:
+// Its pre-condition checks that a question is waiting to be answered.
+// Its post-condition checks that the answer can be handled.
+func (q *Quiz) ReadInput(promptCh <-chan *qa, donePromptCh chan struct{}) {
+	var s string
+	for {
+		promptingQuestion, ok := <-promptCh // wait for a question to prompt for
+		if !ok {
+			return // No more questions to prompt for.
+		}
+
+		fmt.Scanln(&s)
+		if q.ops == nil {
+			return // Quiz has terminated whilst waiting for input.
+		}
+		q.ops <- q.QuestionAnswered(promptingQuestion, s, donePromptCh)
+	}
+}
+
+// QuestionAnswered is an action that stores the given answer
+// into the quiz map of answered questions.
+// It then sends notification of completion over its done chan.
+func (q *Quiz) QuestionAnswered(
+	promptingQuestion *qa,
+	answer string,
+	donePromptCh chan<- struct{}) func() bool {
+
+	return func() bool {
+		q.answered[promptingQuestion] = answer
+		donePromptCh <- struct{}{} // notify next question can be asked
+		return false
+	}
+}
+
+// AskQuestion is an action that prints the question to stdout.
+// It then sends notification that the question is waiting to
+// to be answered using the askCh chan.
+func (q *Quiz) AskQuestion(question *qa, askCh chan<- *qa) func() bool {
+	return func() bool {
+		fmt.Printf("%s= ", question.question)
+		askCh <- question
+		return false
+	}
+}
+
+// PrintSummary is an action that computes the score and prints
+// out a summary.
+// The return value of true indicates that this should be the
+// last operation in the actor loop.
+func (q *Quiz) PrintSummary() func() bool {
+	return func() bool {
+		var correct int
+		for k, v := range q.answered {
+			if k.answer == strings.Trim(v, " ") {
+				correct++
+			}
+		}
+		fmt.Printf("You got %d/%d correct!\n", correct, len(q.qas))
+		return true
+	}
+}
+
+// Run starts the main actor loop.
+// It starts two interacting goroutines used for printing questions
+// and waiting for answers via stdin.
+func (q *Quiz) Run() {
+	promptCh := make(chan *qa)
+	donePromptCh := make(chan struct{})
+
+	defer func() {
+		close(q.ops)
+		close(promptCh)
+		close(donePromptCh)
+	}()
+
+	go q.ReadInput(promptCh, donePromptCh)
+
+	go func() {
+		for _, question := range q.qas {
+			q.ops <- q.AskQuestion(question, promptCh)
+			if _, ok := <-donePromptCh; !ok { // wait until question has been answered.
+				return // timed out waiting for input.
+			}
+		}
+		q.ops <- q.PrintSummary() // all questions have been answered.
+	}()
+
+loop:
+	for {
+		select {
+		case op := <-q.ops:
+			if done := op(); done {
+				break loop
+			}
+		case <-q.ctx.Done():
+			fmt.Println("Times up!")
+			q.PrintSummary()() // Bypass the ops channel and execute op directly.
+			break loop
+		}
+	}
+}
+
 func main() {
-	/*
-		  Program description:
-		  	The quiz accepts a CSV file and allows for a custom timeout and
-			randomized order of asking.
-
-			The implementation is channel-based:
-
-			[question array] -> questions -> ask -> answered -> done
-
-			A done channel allows graceful exit for when all questions have been
-			asked, or a timeout has occured.
-	*/
 	const (
-		defaultCSV     = "problems.csv"
+		defaultCSV     = "./problems.csv"
 		defaultTimeout = 30
 	)
 	var (
@@ -160,22 +207,22 @@ func main() {
 	flag.BoolVar(&shuffle, "shuffle", false, "Shuffle the questions")
 	flag.Parse()
 
-	quiz := NewQuiz(timeoutArg)
-	quiz.readFile(csvArg)
+	quiz := NewQuiz(context.Background())
+	quiz.LoadFromFile(csvArg)
 
 	if shuffle {
 		quiz.shuffle()
 	}
 
+	var cancel context.CancelFunc
 	if timeoutArg > 0 {
-		fmt.Println("Press any key to start...")
+		fmt.Println("Press enter to start...")
 		fmt.Fscanf(os.Stdin, "%s")
-
-		go quiz.timer()
+		var timeoutCtx context.Context
+		timeoutCtx, cancel = context.WithTimeout(quiz.ctx, time.Duration(timeoutArg)*time.Second)
+		quiz.ctx = timeoutCtx
+		defer cancel()
 	}
-	go quiz.processQuestions()
-	go quiz.askQuestion()
 
-	<-quiz.done
-	quiz.printSummary()
+	quiz.Run()
 }
