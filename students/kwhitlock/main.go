652fc7e0296e38ff812efc694a724385db657139
Keir Whitlock <keir.whitlock@stepstone.co.uk>
2019-07-26 15:23:28 +0100

    Quick hack up of Part 1s requirements.

--- students/kwhitlock/main.go
+++ students/kwhitlock/main.go
@@ -5,34 +5,60 @@ import (
 	"os"
 	"bufio"
 	"log"
+	"strings"
+	"flag"
 )
 
-func sendQuestion (text string) {
-	fmt.Println("New question: ")
-	fmt.Println(text)
+var totalScore = 0
+var numberQuestionsAsked = 0
+
+func getUserAnswer() (text string) {
+	reader := bufio.NewReader(os.Stdin)
+	answer, _ := reader.ReadString('\n')
+	return answer
+}
+
+func sendQuestion(text string) (answer string) {
+	questionAnswer := strings.Split(text, ",")
+	fmt.Println(questionAnswer[0])
+	numberQuestionsAsked++
+	return questionAnswer[1]
+}
+
+func compareAnswers(answer, userAnswer string) {
+	if answer == strings.TrimRight(userAnswer, "\n") {
+		fmt.Println("Correct!")
+		totalScore++
+	} else {
+		fmt.Println("Incorrect :o(")
+	}
 }
 
 func main(){
+	questions := flag.String("questions", "./problems.csv", "Filepath/name of CSV file where the questions are stored.")
+	flag.Parse()
+
 	fmt.Println("Running Quiz")
 
-	file, err := os.Open("./problems.csv")
+	file, err := os.Open(*questions)
 	if err != nil {
 		fmt.Println("Could not open file")
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file) // https://stackoverflow.com/questions/8757389/reading-file-line-by-line-in-go
+	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
-		sendQuestion(scanner.Text())
+		answer := sendQuestion(scanner.Text())
+		userAnswer := getUserAnswer()
+		compareAnswers(answer, userAnswer)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("ERROR")
 		log.Fatal(err)
 	}
-	fmt.Println("End of quiz")
-
-
+	fmt.Printf("**End of quiz**\nYou scored: %v/%v\n", totalScore, numberQuestionsAsked)
 
 }
