ec8499ee08bd4752a713ea70becd7ece6e32efbf
bilawalahmed <blwal7057@gmail.com>
2022-05-09 18:16:18 +0500

    Part b done, create class to pass to question method, and used timeout to provide maximum number of seconds to answer.

--- solution.rb
+++ solution.rb
@@ -1,35 +1,63 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'timeout'
 
-def ask_question_check_answer(question, answer)
-  print question, '? '
-  answer_from_user = gets.strip
-
-  return true if answer_from_user == answer
-
-  false
-end
-
-def main(file_name = 'problems.csv')
-  points = 0
-
-  questions = CSV.read file_name
-  questions.each do |question|
-    next if question.length != 2
-
-    statement = question[0].strip
-    answer = question[1].strip
-
-    points += 1 if ask_question_check_answer statement, answer
+# Class for keeping track for points
+class Point
+  def initialize
+    @value = 0
   end
 
-  print "\n"
-  print 'Your score: ', points, '/', lines.length, "\n"
+  def inc
+    @value += 1
+  end
+
+  def get
+    @value
+  end
 end
 
-if ARGV.length >= 1
-  main ARGV[0]
-else
-  main
+def ask_question_check_answer(question, answer, point)
+  print question, '? '
+  answer_from_user = $stdin.gets.strip
+
+  point.inc if answer_from_user == answer
 end
+
+def main(file_name, secs)
+  point = Point.new
+  questions = CSV.read file_name
+
+  print "You will have maximum of #{secs} seconds to answer all questions\n"
+  print 'Press enter to start...'
+  _ = $stdin.gets
+
+  begin
+    Timeout.timeout(secs) do
+      questions.each do |question|
+        next if question.length != 2
+
+        statement = question[0].strip
+        answer = question[1].strip
+
+        ask_question_check_answer statement, answer, point
+      end
+    end
+  rescue Timeout::Error
+  ensure
+    print "\n"
+    print 'Your score: ', point.get, '/', questions.length, "\n"
+  end
+end
+
+file_name = 'problems.csv'
+file_name = ARGV[0] if ARGV.length >= 1
+
+begin
+  timer = Integer(ARGV[1]) if ARGV.length >= 2
+rescue TypeError
+  timer = 30
+end
+
+main file_name, timer
